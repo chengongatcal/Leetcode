646af0db5680b3f73338954fdd7e34b1f34cb157
Chen <chengong@berkeley.edu>
2016-04-13 00:13:04 -0700

    April 13

--- Set_Matrix_Zeroes.java
+++ Set_Matrix_Zeroes.java
@@ -1,6 +1,6 @@
-73. Set Matrix Zeroes
+/** 73. Set Matrix Zeroes
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
-
+*/
 
 public class Solution {
     public void setZeroes(int[][] matrix) {
--- unique_paths.java
+++ unique_paths.java
@@ -1,7 +1,7 @@
 /** A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 
 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below). 
-*/
+
 
 illustration
    0  1  2
@@ -9,7 +9,7 @@ illustration
 0| 1  1  1 |
 1| 1  2  3 |
 2| 1  3  6 |
-
+ */
 
 public class Solution {
     public int uniquePaths(int m, int n) {
